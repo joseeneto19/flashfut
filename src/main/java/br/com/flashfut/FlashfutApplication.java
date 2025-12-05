92f2fd203f75b7173732b1da4df120d7be56548b
joseeneto19 <josemnetodev@gmail.com>
2025-12-05 13:54:39 -0300

    first commit

---
+++ src/main/java/br/com/flashfut/FlashfutApplication.java
@@ -0,0 +1,13 @@
+package br.com.flashfut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashfutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashfutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flashfut/FlashfutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flashfut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashfutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
