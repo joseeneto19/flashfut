5a675fdf64c229273ea4bc6c4bdb4af2b656ca01
joseeneto19 <josemnetodev@gmail.com>
2025-12-05 14:01:02 -0300

    feat: cria entidades jogador e time

---
+++ src/main/java/br/com/flashfut/entities/Jogador.java
@@ -0,0 +1,14 @@
+package br.com.flashfut.entities;
+
+import java.time.LocalDate;
+
+public class Jogador {
+
+    private String nome;
+    private LocalDate dataDeNascimento;
+    private Time time;
+    private Double altura;
+    private Double peso;
+    private String paisDeNascimento;
+    private Integer numeroDaCamisa;
+}
---
+++ src/main/java/br/com/flashfut/entities/Time.java
@@ -0,0 +1,11 @@
+package br.com.flashfut.entities;
+
+import java.util.List;
+
+public class Time {
+
+    private String nome;
+    private String estadio;
+    private String mascote;
+    private List<Jogador> jogadores;
+}
