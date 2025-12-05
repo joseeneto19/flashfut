de1a7057b0e860cebd27e3964f11246af8ed00d7
joseeneto19 <josemnetodev@gmail.com>
2025-12-05 14:29:54 -0300

    feat: cria a v1 do banco de dados via migration e configura o banco de dados no application.properties

--- src/main/resources/db/migration/V1__cria_entidades_time_jogador.sql
+++ src/main/resources/db/migration/V1__cria_entidades_time_jogador.sql
@@ -1,3 +1,21 @@
-CREATE TABLE tb_time(
+CREATE TABLE tb_time (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    estadio VARCHAR(255),
+    mascote VARCHAR(255)
+);
 
-)
+CREATE TABLE tb_jogador (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    data_de_nascimento DATE,
+    time_id BIGINT,
+    altura DOUBLE,
+    peso DOUBLE,
+    pais_de_nascimento VARCHAR(255),
+    numero_da_camisa INT,
+
+    CONSTRAINT fk_jogador_time
+    FOREIGN KEY (time_id)
+    REFERENCES tb_time(id)
+);
