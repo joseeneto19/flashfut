8b47deb963d0fa18db6ede59130d11667e3f5259
joseeneto19 <josemnetodev@gmail.com>
2025-12-05 14:09:45 -0300

    feat: cria relacionamentos entre as entidades

--- src/main/java/br/com/flashfut/entities/Jogador.java
+++ src/main/java/br/com/flashfut/entities/Jogador.java
@@ -1,11 +1,29 @@
 package br.com.flashfut.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_jogaodr")
 public class Jogador {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
     private String nome;
     private LocalDate dataDeNascimento;
+    @ManyToOne
+    @JoinColumn(name = "time_id")
     private Time time;
     private Double altura;
     private Double peso;
--- src/main/java/br/com/flashfut/entities/Time.java
+++ src/main/java/br/com/flashfut/entities/Time.java
@@ -1,11 +1,28 @@
 package br.com.flashfut.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_time")
 public class Time {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
     private String nome;
     private String estadio;
     private String mascote;
+    @OneToMany(mappedBy = "time")
     private List<Jogador> jogadores;
 }
