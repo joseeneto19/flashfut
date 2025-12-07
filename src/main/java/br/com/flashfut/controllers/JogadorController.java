e27a1912dc01b06b515f5cbd1d66e41b0d24bb4e
joseeneto19 <josemnetodev@gmail.com>
2025-12-07 20:24:36 -0300

    feat: cria os metodos e endpoints de criar jogador e listar todos jogadores

--- src/main/java/br/com/flashfut/controllers/JogadorController.java
+++ src/main/java/br/com/flashfut/controllers/JogadorController.java
@@ -1,4 +1,27 @@
 package br.com.flashfut.controllers;
 
+import br.com.flashfut.entities.Jogador;
+import br.com.flashfut.services.JogadorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jogadores")
+@RequiredArgsConstructor
 public class JogadorController {
+
+    private final JogadorService service;
+
+    @GetMapping
+    public List<Jogador> listarJogadores() {
+        return service.listarJogadores();
+    }
+
+    @PostMapping
+    public Jogador criarJogador(@RequestBody Jogador jogador) {
+        return service.criarJogador(jogador);
+    }
+
 }
--- src/main/java/br/com/flashfut/services/JogadorService.java
+++ src/main/java/br/com/flashfut/services/JogadorService.java
@@ -1,4 +1,24 @@
 package br.com.flashfut.services;
 
+import br.com.flashfut.entities.Jogador;
+import br.com.flashfut.repositories.JogadorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class JogadorService {
+
+
+    private final JogadorRepository repository;
+
+    public List<Jogador> listarJogadores() {
+        return repository.findAll();
+    }
+
+    public Jogador criarJogador(Jogador jogador) {
+        return repository.save(jogador);
+    }
 }
